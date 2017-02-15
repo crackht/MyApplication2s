ba25da249b5be7a3032242095254ff1222f68ee5
briliantwy <v5wy007@163.com>
2017-02-15 15:23:04 +0800

    Initial commit

---
+++ app/src/main/java/com/example/sks/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.sks.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //这是修改这是第二次修改
+    }
+}
